c14a7264214c32e37284cf5d4cf0763bf5dbb740
Esther Arias <estherarias88@gmail.com>
2023-03-07 19:32:37 +0100

    Initial commit

---
+++ app/src/main/java/com/esardo/a2ndhand/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.esardo.a2ndhand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
