2f6def0c614f6bdca0d2aca06cf837ce59b95d37
Esther Arias <estherarias88@gmail.com>
2023-03-11 19:32:39 +0100

    MainActivity completed (without styles)

---
+++ app/src/main/java/com/esardo/a2ndhand/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.esardo.a2ndhand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/esardo/a2ndhand/MainActivity.kt
+++ app/src/main/java/com/esardo/a2ndhand/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.esardo.a2ndhand
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.esardo.a2ndhand.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(ActivityMainBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        //When the button is clicked it will load the DrawerActivity
+        binding.btnLogIn.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
     }
 }
