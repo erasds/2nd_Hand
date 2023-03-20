e87a52e96030655b317e4ad6f9a3f61f68a5052f
Esther Arias <estherarias88@gmail.com>
2023-03-20 21:10:44 +0100

    Product Fragment

--- app/src/main/java/com/esardo/a2ndhand/UploadProduct.kt
+++ app/src/main/java/com/esardo/a2ndhand/UploadProduct.kt
@@ -1,11 +1,20 @@
 package com.esardo.a2ndhand
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.esardo.a2ndhand.databinding.ActivityUploadProductBinding
 
 class UploadProduct : AppCompatActivity() {
+    private lateinit var binding: ActivityUploadProductBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upload_product)
+        setContentView(ActivityUploadProductBinding.inflate(layoutInflater).also { binding = it }.root)
+
+        binding.btnUpload.setOnClickListener {
+            //Insertar datos
+            finish()
+        }
     }
 }
